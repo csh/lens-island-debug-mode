4638bb43a6768038e6b166e90aad7c001dc1e75a
csh <csh@smrkn.com>
2025-07-11 18:38:16 +0100

    Initial impl

---
+++ Plugin.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace LensIslandDebugMode;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class DebugModePlugin : BaseUnityPlugin
+{
+    private Harmony _harmony;
+
+    private void Awake()
+    {
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} is loaded!");
+
+        _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        _harmony.PatchAll();
+    }
+
+    private void OnDestroy()
+    {
+        _harmony?.UnpatchSelf();
+    }
+    
+    [HarmonyPatch(typeof(DebugConsole), nameof(DebugConsole.DevBuild), MethodType.Getter)]
+    class Patch_DevBuild
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(DebugConsole), nameof(DebugConsole.DevPlayingLiveBuild), MethodType.Getter)]
+    class Patch_DevPlayingLiveBuild
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(DebugConsole), nameof(DebugConsole.DebugCommandAllowed))]
+    class Patch_DebugCommandAllowed
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Debug Mode
+
+Developer commands are executed in chat, same as regular ones.
+
+Side effects unknown. This exists to facilitate modding the game.
