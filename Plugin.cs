87d3c7c00823cd8d414d037e7c68a7a8edb68705
csh <csh@smrkn.com>
2025-07-24 15:29:44 +0100

    Make application of patches configurable
    Default to false

--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 
 namespace LensIslandDebugMode;
@@ -7,19 +9,60 @@ namespace LensIslandDebugMode;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class DebugModePlugin : BaseUnityPlugin
 {
+    private ConfigEntry<bool> _enablePatches;
     private Harmony _harmony;
-
+    
     private void Awake()
     {
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} is loaded!");
 
-        _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        /*
+         * Default to false so that if somebody forgets to disable the mod before joining
+         * a server any unknown side effects can be prevented.
+         */
+        _enablePatches = Config.Bind("Debug Mode", "Enable", false);
+        
+        _enablePatches.SettingChanged += ToggleDebugMode;
+
+        if (_enablePatches.Value)
+        {
+            ApplyPatch();
+        }
+    }
+
+    private void ToggleDebugMode(object sender, EventArgs e)
+    {
+        if (_enablePatches.Value)
+        {
+            ApplyPatch();
+        }
+        else
+        {
+            RevertPatch();
+        }
+    }
+
+    private void ApplyPatch()
+    {
+        if (_harmony is not null) return;
+        _harmony = new  Harmony(MyPluginInfo.PLUGIN_GUID);
         _harmony.PatchAll(typeof(DebugConsolePatches));
+    }
+
+    private void RevertPatch()
+    {
+        if (_harmony is null) return;
+        _harmony.UnpatchSelf();
+        _harmony = null;
     }
 
     private void OnDestroy()
     {
-        _harmony?.UnpatchSelf();
+        if (_enablePatches is not null)
+        {
+            _enablePatches.SettingChanged -= ToggleDebugMode;
+        }
+        RevertPatch();
     }
 
     public static class DebugConsolePatches
